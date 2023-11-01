f375efe9746624ad79c5c84a13d31617bbd52aa8
dioswilson <agustinordonez15@gmail.com>
2023-11-01 00:20:25 -0300

    Terminal not opening on insert mode

--- lua/dioswilson/plugins/terminal.lua
+++ lua/dioswilson/plugins/terminal.lua
@@ -6,7 +6,7 @@ return {
             open_mapping = [[<leader>t]],
             start_in_insert = true,
             -- terminal_mappings = true,
-            -- insert_mappings = true, -- whether or not the open mapping applies in insert mode
+            insert_mappings = false, -- whether or not the open mapping applies in insert mode
             direction = "horizontal",
             close_on_exit = true, -- close the terminal window when the process exits
         })
