8efbba6664922f68f764eafbb638d00fcd088125
dioswilson <agustinordonez15@gmail.com>
2023-12-10 22:58:14 -0300

    Solved an error with copilot

--- lua/dioswilson/plugins/githubCopilot.lua
+++ lua/dioswilson/plugins/githubCopilot.lua
@@ -1,6 +1,8 @@
 --Maybe shouldn't have it here?
+-- TODO: FIGURE THIS OUT
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.g.copilot_assume_mapped = true
 return {
-    "github/copilot.vim"
+    "github/copilot.vim",
 }
