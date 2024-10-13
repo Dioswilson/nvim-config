82920a5ed23a66a9e18a424f0c2a10a5118c0d3d
dioswilson <agustinordonez15@gmail.com>
2024-10-13 17:55:20 -0300

    Persisted loads session if called with argument

--- lua/dioswilson/plugins/persisted.lua
+++ lua/dioswilson/plugins/persisted.lua
@@ -1,5 +1,6 @@
 return {
     "olimorris/persisted.nvim",
+
     config = function()
         require("persisted").setup({
             autoload = true,
@@ -14,6 +15,34 @@ return {
         require("telescope").load_extension("persisted")
         require("dioswilson.keymap.persisted")
 
+		
+        -- Load session on startup with arguments (By https://github.com/neandrake)
+        local persisted = require("persisted")
+        vim.api.nvim_create_autocmd("VimEnter", {
+            nested = true,
+            callback = function()
+                if vim.g.started_with_stdin then
+                    return
+                end
+
+                local forceload = false
+                if vim.fn.argc() == 0 then
+                    forceload = true
+                elseif vim.fn.argc() == 1 then
+                    local dir = vim.fn.expand(vim.fn.argv(0))
+                    if dir == "." then
+                        dir = vim.fn.getcwd()
+                    end
+
+                    if vim.fn.isdirectory(dir) ~= 0 then
+                        forceload = true
+                    end
+                end
+
+                persisted.autoload({ force = forceload })
+            end,
+        })
+
         vim.api.nvim_create_autocmd("User", {
             pattern = "PersistedSavePre",
             callback = function()
@@ -21,13 +50,15 @@ return {
                 require("neo-tree.command").execute({ action = "close" })
             end,
         })
-
-        vim.api.nvim_create_autocmd("VimEnter", {
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "PersistedLoadPost",
             callback = function()
-                if vim.fn.isdirectory(vim.fn.expand("%:p")) == 1 then
-                    vim.cmd("SessionLoad")
+                --Attempts to solve neotree being opened when opening a directory
+                if require("neo-tree.setup.netrw").get_hijack_netrw_behavior() ~= "disabled" then
+                    require("neo-tree.command").execute({ action = "close"})
                 end
             end,
         })
+
     end,
 }
